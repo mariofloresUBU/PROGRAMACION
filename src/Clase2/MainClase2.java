8ab1942bcdd4f5cf2b03ccf9f5f0adfd7edfee8b
mariofloresUBU <mfc1027@alu.ubu.es>
2025-03-05 19:33:47 +0100

    Subida clase2 para sincronizar repositorio.

---
+++ src/Clase2/MainClase2.java
@@ -0,0 +1,15 @@
+package Clase2;
+
+public class MainClase2 {
+    public static void main(String[] args) {
+    //Expresiones y/o evaluaciones
+        int resultado = (10 + 5) * 2/3;
+        int resultado2 = 10+5*2/3;
+        System.out.println("El resultado de la expresión es: " + resultado);
+        System.out.println("El resultado de la expresión es: " + resultado2);
+
+
+
+    }
+
+}
